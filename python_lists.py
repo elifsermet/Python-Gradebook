f63fd929b0e9ad40fe981e391325524a0de42837
elifsermet <elifsermet93@gmail.com>
2019-03-06 23:08:38 -0600

    Add new subject and grade

--- python_lists.py
+++ python_lists.py
@@ -4,9 +4,9 @@ grades = [98, 97, 85, 88]
 subjects.append('computer science')
 grades.append(100)
 
-
 gradebook = list(zip (subjects,grades))
 gradebook.append(('visual arts', 93))
+gradebook.append(('biology', 100))
 
 print(gradebook)
 
